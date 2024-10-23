2e5e121fc2a146ea64db9aa89a4b46f1c77e4193
Andrew <bayledgeacademy@icloud.com>
2024-10-22 22:18:20 -0500

    task 4

---
+++ COSC3337F24-PS2T4-Andrew-Lee.py
@@ -0,0 +1,10 @@
+# data <- read.csv("weather_data.csv")
+
+# DATE / nominal / Each record has a date starting from 01/01/2021 to 12/31/2021
+# CLOUDCOVER / categorical / %/ 17 different types of cloud cover. Categories are: 
+#    “Fair”, “Fair / Windy", "Partly Cloudy", "Partly Cloudy / Windy", "Cloudy", "Cloudy / Windy","Mostly Cloudy","Mostly Cloudy / Windy","Fog","Haze", "Light Rain",  "Light Rain with Thunder", "Thunder", "Rain" "Thunder / Windy"  "Heavy T-Storm", "Thunder in the Vicinity", "T-Storm"
+# RAINFALL / continuous / inch / Amount of rainfall of the day/ from 0 to 5
+# MIN TEMP / continuous / Fahrenheit / Minimum temperature at 3pm / from 34 to 83
+# WIND SPEED / continuous / mile per hour / wind speed at 3pm/ from 0 to 29
+# HUMIDITY / continuous / % / Humidity at 3pm/ from 0 to 100
+
